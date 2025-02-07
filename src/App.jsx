c5365b50b9372c51e5685c469d122a048aead329
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-02-07 16:25:43 +0100

    feat: add components for: header, footer, default layout, about us, contacts, posts list, home page, page not found. import every component in their destination

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// layouts
+import DefaultLayout from "./layouts/DefaultLayout";
+// pages
+import HomePage from "./pages/HomePage";
+import PostList from "./pages/PostsList";
+import AboutUs from "./pages/AboutUs";
+import Contacts from "./pages/Contacts";
+import PageNotFound from "./pages/PageNotFound";
+
 export default function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/posts-list" element={<PostList />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>React Blog</h1>
+    </header>
+  );
+}
