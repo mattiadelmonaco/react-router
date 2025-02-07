59c519fce0d81d464dda0a7b3ba460518f664a40
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-02-07 21:34:14 +0100

    fix: change name to files with same name, now have PostsListPage and PostsListComp

--- src/pages/PostsListPage.jsx
+++ src/pages/PostsListPage.jsx
@@ -1,11 +1,7 @@
-// <h1 className="text-4xl text-center font-bold text-red-900">
-//   I NOSTRI POST
-// </h1>
-
 import { useState, useEffect } from "react";
 
 import axios from "axios";
-import PostsList from "../components/PostsList";
+import PostsList from "../components/PostsListComp";
 
 export default function PostList() {
   const [postData, setPostData] = useState([]);
