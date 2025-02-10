9c7b3245f4118b14b95f59552f56cbf16d58dbb4
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-02-10 09:47:44 +0100

    fix: move key from li to link - delete margin to header and footer

--- src/components/PostsListComp.jsx
+++ src/components/PostsListComp.jsx
@@ -5,11 +5,8 @@ export default function PostsListComp({ postData }) {
     <ul className="grid grid-cols-3 gap-10 w-6xl mx-auto text-white">
       {postData.map((post) => {
         return (
-          <Link to={"#"}>
-            <li
-              key={post.id}
-              className="bg-red-950 h-[400px] rounded-3xl flex flex-col items-center p-4 shadow-2xl"
-            >
+          <Link to={"#"} key={post.id}>
+            <li className="bg-red-950 h-[400px] rounded-3xl flex flex-col items-center p-4 shadow-2xl">
               <div className="w-[200px] aspect-video object-cover my-3">
                 <img
                   className="size-full rounded-2xl"
